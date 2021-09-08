489f8f1463713690df88d9c1f8eaf36d9d32c0fb
Ivica Lovric <37185376+ivlovric@users.noreply.github.com>
2021-09-08 12:37:23 +0200

    Create prometheus.go

---
+++ prometheus.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var connectedClients = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "hfp_client_connects_in",
+		Help: "No of inbound client connects",
+	},
+)
+
+var connectionStatus = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "hfp_connection_status_out",
+		Help: "Connection status OUT - 1 is connected, 0 is disconnected",
+	},
+)
+
+var nonHEPPackets = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "hfp_non_hep_packets_in",
+		Help: "No of inbound NON HEP pakets",
+	},
+)
+
+var hepBytesInFile = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "hfp_hep_bytes_in_file",
+		Help: "No of HEP bytes in file",
+	},
+)
+
+var hepFileFlushesSuccess = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "hfp_hep_file_flushes_success",
+		Help: "No of times HEP pakets from file have been successfully sent over network to backend HEP server",
+	},
+)
+
+var hepFileFlushesError = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "hfp_hep_file_flushes_error",
+		Help: "No of times HEP pakets from file failed sending over network to backend HEP server",
+	},
+)
+
+func startMetrics(wg *sync.WaitGroup) {
+	prometheus.MustRegister(connectedClients)
+	prometheus.MustRegister(connectionStatus)
+	prometheus.MustRegister(nonHEPPackets)
+	prometheus.MustRegister(hepBytesInFile)
+	prometheus.MustRegister(hepFileFlushesSuccess)
+	prometheus.MustRegister(hepFileFlushesError)
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":"+*PrometheusPort, nil)
+	wg.Done()
+
+}
